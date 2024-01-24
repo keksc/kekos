1c07c4289b362baaf4beac08e3b32c2a8c22a0bc
keksc <pboursin2008@orange.fr>
2024-01-24 07:36:02 +0100

    removed A20 shit

--- src/bootloader/stage2/entry.asm
+++ src/bootloader/stage2/entry.asm
@@ -21,7 +21,7 @@ entry:
     mov bp, sp
 
     ; switch to protected mode
-    call EnableA20          ; 2 - Enable A20 gate
+    ;call EnableA20          ; 2 - Enable A20 gate
     call LoadGDT            ; 3 - Load GDT
 
     ; 4 - set protection enable flag in CR0
@@ -58,74 +58,11 @@ entry:
     cli
     hlt
 
-
-EnableA20:
-    [bits 16]
-    ; disable keyboard
-    call A20WaitInput
-    mov al, KbdControllerDisableKeyboard
-    out KbdControllerCommandPort, al
-
-    ; read control output port
-    call A20WaitInput
-    mov al, KbdControllerReadCtrlOutputPort
-    out KbdControllerCommandPort, al
-
-    call A20WaitOutput
-    in al, KbdControllerDataPort
-    push eax
-
-    ; write control output port
-    call A20WaitInput
-    mov al, KbdControllerWriteCtrlOutputPort
-    out KbdControllerCommandPort, al
-    
-    call A20WaitInput
-    pop eax
-    or al, 2                                    ; bit 2 = A20 bit
-    out KbdControllerDataPort, al
-
-    ; enable keyboard
-    call A20WaitInput
-    mov al, KbdControllerEnableKeyboard
-    out KbdControllerCommandPort, al
-
-    call A20WaitInput
-    ret
-
-
-A20WaitInput:
-    [bits 16]
-    ; wait until status bit 2 (input buffer) is 0
-    ; by reading from command port, we read status byte
-    in al, KbdControllerCommandPort
-    test al, 2
-    jnz A20WaitInput
-    ret
-
-A20WaitOutput:
-    [bits 16]
-    ; wait until status bit 1 (output buffer) is 1 so it can be read
-    in al, KbdControllerCommandPort
-    test al, 1
-    jz A20WaitOutput
-    ret
-
-
 global LoadGDT
 LoadGDT:
     [bits 16]
     lgdt [g_GDTDesc]
     ret
-
-
-
-KbdControllerDataPort               equ 0x60
-KbdControllerCommandPort            equ 0x64
-KbdControllerDisableKeyboard        equ 0xAD
-KbdControllerEnableKeyboard         equ 0xAE
-KbdControllerReadCtrlOutputPort     equ 0xD0
-KbdControllerWriteCtrlOutputPort    equ 0xD1
 
 ScreenBuffer                        equ 0xB8000
 
